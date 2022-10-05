6bc2d926d5c088db0196d8b19b34a1aba43a0167
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2022-10-05 01:15:39 -0300

    change button removed and functon

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,24 +1,24 @@
 import React, { useState } from 'react';
 import { MdAddCircle } from 'react-icons/md';
-import { FaTrashAlt } from 'react-icons/fa';
+
 import { BiSend } from 'react-icons/bi';
 import { IoMdArrowRoundBack } from 'react-icons/io';
 import { Link } from 'react-router-dom';
+//bootstrap
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 import './Task.css';
 import { UseTasks } from '../../contexts/TasksContext';
-import ButtonRemove from '../buttons/ButtonRemove';
 
 export const Tasks = () => {
   const storageTasks = JSON.parse(localStorage.getItem('tasksSaveUsers'));
   const { setTasks } = UseTasks();
 
   const handleRemoveTask = (id) => {
-    const confirmDelete = confirm('Deseja deletar esse item ?');
-    if (confirmDelete) {
       const remove = storageTasks.filter(checkedId => checkedId.id != id);
       localStorage.setItem("tasksSaveUsers", JSON.stringify(remove));
       return setTasks(remove);
-    }
   }
 
   return (
@@ -31,6 +31,7 @@ export const Tasks = () => {
         <Link to='/createdTask' className='add_mew_task'>
           <MdAddCircle />
         </Link>
+
       </div>
 
       <div className='container_tasks'>
@@ -38,9 +39,7 @@ export const Tasks = () => {
           <div key={task.id} className='task_box_list_with_title'>
             <h4>{task.title}</h4>
             <div className='flex_options'>
-
-              <ButtonRemove/>
-
+             
               <Link className='send_task' to={`/viewtask/${task.id}`}>
                 <BiSend />
               </Link>
--- src/pages/viewTask/ViewTask.jsx
+++ src/pages/viewTask/ViewTask.jsx
@@ -1,19 +1,31 @@
 import React, { useState } from 'react'
 import { Link, useParams } from 'react-router-dom';
 import { IoMdArrowRoundBack } from 'react-icons/io';
-import { ImCheckboxUnchecked, ImCheckboxChecked } from 'react-icons/im';
+import { FaTrashAlt } from 'react-icons/fa';
 import './ViewTask.css';
+import { UseTasks } from '../../contexts/TasksContext';
 
 export const ViewTask = () => {
     const { id } = useParams();
-    const [mark, setMarkCompleted] = useState(false);
 
-    const storageTasks = JSON.parse(localStorage.getItem('tasksSaveUsers'))
+    const storageTasks = JSON.parse(localStorage.getItem('tasksSaveUsers'));
+    const { setTasks } = UseTasks();
+  
+    const handleRemoveTask = (id) => {
+        const remove = storageTasks.filter(checkedId => checkedId.id != id);
+        localStorage.setItem("tasksSaveUsers", JSON.stringify(remove));
+        window.location.href = '/tasks'; 
+        
+        return setTasks(remove);    
+
+    }
+
+    const storageTasksId = JSON.parse(localStorage.getItem('tasksSaveUsers'))
         .filter(list => list.id == id);
 
     return (
         <div>
-            {storageTasks.map(task => (
+            {storageTasksId.map(task => (
                 <div key={task.id} className='task_box'>
                     <div className='flex_btn_title'>
                         <Link className='back_task' to='/tasks'>
@@ -24,6 +36,10 @@ export const ViewTask = () => {
                     <h2 className='title'>{task.title}</h2>
                     <p className='description'>{task.description}</p>
                     <span className='date'>{task.date}</span>
+
+                    <button className='btn_trash' onClick={() => handleRemoveTask(task.id)}>
+                <FaTrashAlt />
+              </button>
                 </div>
             ))}
         </div>
